2604335464e17097633c01ca605fbea7e1669977
Anup Mishra <anupm0873@gmail.com>
2024-04-21 22:36:04 +0530

    makind edit and delete page

--- REST/index.js
+++ REST/index.js
@@ -1,58 +1,83 @@
-const express=require("express");
+const express=require("express");//for import express
 const app=express();
-
+const methodoverride=require("method-override");//import to change ejs method
 const port=8080;
 const path =require("path");
-
+const {v4: uuidv4}=require("uuid");//import to create unique id
+app.use (methodoverride("_method"));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
-app.use(express.static(path.join(__dirname,"/public")));
-app.use(express.urlencoded({extended:true}));
-
+app.use(express.static(path.join(__dirname,"/public")));//to use public file
+app.use(express.urlencoded({extended:true}));// to take respond from post
+// post content
 let posts=[
     {
-        id:"1a",
+        id: uuidv4(),
         name:"anup",
         content:"i love coading",
     },
     { 
-        id:"2b",
+        id:uuidv4() ,
         name:"ansh",
         content:"i explore new things",
     },
     {
-        id:"3c",
+        id:uuidv4(),
         name:"mansi",
         content:"I got selected for internship",
     },
     {
-        id:"4d",
+        id:uuidv4(),
         name:"vishesh",
         content:"hardworking is important to achive success",
     },
 ]
-
+// send responce to post page request
 app.get("/post",(req,res)=>{
     res.render("index.ejs", {posts});
 })
-
+// send responce to post new page request
 app.get("/post/new",(req,res)=>{
     res.render("new.ejs");
 })
-
+//to generate post id page
 app.get("/post/:id",(req,res)=>{
     let {id}=req.params;
     let post=posts.find((p)=> id===p.id);
-    res.render("show.ejs",{post})
-    // res.send("id is woeking")
+    res.render("show.ejs",{post})                           
 })
-
+// to generate unique id
 app.post("/post",(req,res)=>{
     let {name ,content}=req.body;
-    posts.push({name ,content});
+    let id=uuidv4();
+    posts.push({id,name,content});
+    res.redirect("/post");
+})
+// for eiditing the post
+app.patch("/post/:id",(req,res)=>{
+    let {id}= req.params;
+    let newcontent=req.body.content;
+    let post=posts.find((p)=> id===p.id);
+    post.content=newcontent;
+    console.log(post);
     res.redirect("/post");
 })
 
+
+// to eiditing post page
+app.get("/post/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post =posts.find((p)=>id===p.id);
+    res.render("edit.ejs",{post});
+})
+
+// for delete the post
+app.delete("/post/:id",(req,res)=>{
+    let {id}=req.params;
+     posts =posts.filter((p)=>id!==p.id);
+    res.redirect("/post");
+});
+// to generater server
 app.listen(port,()=>{
     console.log("listen to the port 8080");
 })

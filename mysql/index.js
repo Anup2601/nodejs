60a3c927ea951081caf8c5c22ab0efb34d7a5b4d
Anup Mishra <anupm0873@gmail.com>
2024-05-02 23:38:00 +0530

    adding sql file

--- mysql/index.js
+++ mysql/index.js
@@ -1,26 +1,33 @@
 const { faker } = require('@faker-js/faker');
-const mysql=require("mysql");
+const mysql=require("mysql2");
 const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'Anup#7070',
+    password : '',
     database : 'my_db'
   });
 
+  let q="insert into user(id,username,email,password)values(?,?,?,?)";
+  let user=["12345","@anupmishra","anupm123@gmail.com","esrdx"];
   try {
-    connection.query("show table",(err,result)=>{
+    connection.query(q,user,(err,result)=>{
         if (err)throw err ;
         console.log(result);
     });
-  } catch (error) {
+  } catch (err) {
     console.log(err);
   }
+
+  connection.end();
 let getuser=()=> {
   return {
-    userId: faker.string.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
+     id:faker.string.uuid(),
+     username:faker.internet.userName(),
+     email:faker.internet.email(),
+     password:faker.internet.password(),
+};
 }
+console.log(getuser());
+
+
  
---
+++ mysql/schema.sql
@@ -0,0 +1,6 @@
+--  CREATE TABLE user(
+--     id=varchar(50)primary key,
+--     username=varchar(50) UNIQUE,
+--     email=varchar(50)UNIQUE NOT NULL,
+--     password=varchar(50)NOT NULL
+--  );
